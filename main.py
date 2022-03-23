2cb2cb07c63261dfce838c7f123365f2a74c036d
bucksmith <96265225+fatiholgunoz@users.noreply.github.com>
2022-03-23 23:22:33 +0000

    Reveal word after failure

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
-#Step 1 
 import random
-word_list = ["aardvark", "baboon", "camel"]
+import hangman_art
+import hangman_words
+from replit import clear
 
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+print(hangman_art.logo)
+chosen_word = hangman_words.word_list[random.randint(0,213)]
+letter_count = len(chosen_word)
 
-chosen_word = word_list[random.randint(0,2)]
-
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
 #Set up a display showing letter slots hidden
 display=[]
-for chars in range(len(chosen_word)):
+for chars in range(letter_count):
   display.append("_")
 
-guess = (input("Guess a letter: ")).lower()
-
 #Compare user guess with letters, reveal found letters in disp.
+#Repeat until no hidden letters left
+  
+hidden_letters = letter_count
+lives = 6
 
-for letter in range(len(chosen_word)):
-  if guess == chosen_word[letter]:
-    display[letter] = chosen_word[letter]
+while hidden_letters > 0 and lives > 0:
+  guess = (input("Guess a letter: ")).lower()
+  clear()
+  for letter in range(letter_count):
+    if guess in display[letter]:
+      print("This letter is already found!")
+    elif guess == chosen_word[letter]:
+      display[letter] = chosen_word[letter]
+      hidden_letters = hidden_letters - 1 
+    
+  if guess not in chosen_word:
+    print(f"The letter '{guess}' is not in the word. ")
+    lives = lives - 1  
+    
+  print(f"{' '.join(display)}")
+  print(f"\n{lives} lives left.")
+  print(hangman_art.stages[lives])
+  
+  if hidden_letters == 0:
+    print("\nYou win!")
+  elif lives == 0:
+    print("\nYou lose!")
+    print(f"The word was {chosen_word}.")
 
-print(display)
